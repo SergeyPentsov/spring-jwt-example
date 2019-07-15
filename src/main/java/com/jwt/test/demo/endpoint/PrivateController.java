60824c5b778cb7cc735f7e770ad74316d96fc5f9
Sergey Pensov <sergey.pensov93@gmail.com>
2019-07-15 21:20:01 +0300

    Implement JWT and Spring Security configuration

---
+++ src/main/java/com/jwt/test/demo/endpoint/PrivateController.java
@@ -0,0 +1,16 @@
+package com.jwt.test.demo.endpoint;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/private")
+public class PrivateController {
+
+    @GetMapping
+    public String getMessage() {
+        return "Hello from private API controller";
+    }
+}
